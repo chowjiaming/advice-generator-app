35ac36a64e9b28ca027b0bf6c47ceacfa97c6905
Joseph Chow <joefstack@gmail.com>
2022-03-26 19:44:38 -0400

    added error console log

--- src/components/Advice/Advice.js
+++ src/components/Advice/Advice.js
@@ -1,19 +1,20 @@
 import React, { Fragment } from 'react'
 
 export default function Advice({ adviceCounter, advice, isLoading, error }) {
+    console.log(error);
     return (
         <Fragment>
-            {isLoading 
-            ? 
-            <Fragment>
-                <div className="card-title">Loading...</div>
-                <div className="advice">Loading....</div>
-            </Fragment> 
-            : 
-            <Fragment>
-                <div className="card-title">Advice Number {advice.slip.id}</div>
-                <div className="advice">{advice.slip.advice}</div>
-            </Fragment>
+            {isLoading
+                ?
+                <Fragment>
+                    <div className="card-title">Loading...</div>
+                    <div className="advice">Loading....</div>
+                </Fragment>
+                :
+                <Fragment>
+                    <div className="card-title">Advice # {advice.slip.id}</div>
+                    <div className="advice">{advice.slip.advice}</div>
+                </Fragment>
             }
         </Fragment>
     )
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -7,7 +7,7 @@ import Button from '../Button/Button'
 import './Card.css'
 
 export default function Card() {
-    const [adviceCounter, setAdviceCounter] = useState(0);
+    const [adviceCounter, setAdviceCounter] = useState(1);
     const { advice, isLoading, error } = useFetchAdvice(adviceCounter);
 
     return (
